0b09751b6e5fff235f4d0d1192c1f8c4b786eee6
Matteo@localhost <matteo.daniele@amadeus.com>
2019-01-08 16:10:57 +0100

    smanettando un po ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template: '<h1>{{title}}</h1>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Danco MS';
+  message = 'Here are some links to help you start:';
+  date: Date = new Date();
 }
