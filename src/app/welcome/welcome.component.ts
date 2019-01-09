6d1337678ff7c6fd78036c89b3430665063794c9
Matteo@localhost <matteo.daniele@amadeus.com>
2019-01-09 17:09:41 +0100

    created login component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { AppComponent } from '../app.component'; //use class from external module
 
 @Component({
   selector: 'app-welcome',
@@ -7,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
   
-  title='The Welcome Component';
+  title='The Welcome Component'
+  message : string = 'Some Welcome Message'
 
   constructor() { 
   }
 
   ngOnInit() {
+    console.log(this.message)
   }
 
 }
